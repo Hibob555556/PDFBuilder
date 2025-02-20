b8ca673495db476033b767c8aa06eac7c72f9243
Cayden Lunt <cayden.lunt@gmail.com>
2025-02-19 23:46:43 -0700

    Initial Commit

---
+++ src/genPdf.js
@@ -0,0 +1,34 @@
+/*  ***************************************
+ *          Author: Cayden Lunt           *
+ *         Creation Date: 2-19-25         *
+ *          Last Update: 2-19-25          *
+ ****************************************** */
+
+// import needed deps
+import fs from "fs";
+import PDFDocument from "pdfkit";
+
+// a class with functions to generate a PDF
+class genPDF {
+  /**
+   * @description Generates and returns a PDFDocument object to modify
+   * @returns {PDFKit.PDFDocument}
+   */
+  createPDF() {
+    const doc = new PDFDocument();                            // create a PDFDocument
+    doc.pipe(fs.createWriteStream("../Output/output.pdf"));   // Pipe the PDF to a file
+    return doc;                                               // return the PDF to be modified
+  }
+
+  /**
+   * @description Save the PDF
+   * @param {PDFKit.PDFDocument} doc
+   * @example genPdf.savePDF(doc);
+   */
+  savePDF(doc) {
+    doc.end();
+  }
+}
+
+// export the class to be used
+export default new genPDF();
---
+++ src/index.js
@@ -0,0 +1,12 @@
+/*  ***************************************
+ *          Author: Cayden Lunt           *
+ *         Creation Date: 2-19-25         *
+ *          Last Update: 2-19-25          *
+ ****************************************** */
+
+// import needed deps
+import genPdf from "./genPdf.js";
+
+let pdf = genPdf.createPDF();                       // Create a PDF to modify    
+pdf.fontSize(25).text('Hello, PDF!', 100, 100);     // Write text to the PDF
+genPdf.savePDF(pdf);                                // save the PDF
